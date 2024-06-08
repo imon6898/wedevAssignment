687cfc74026e1387fb1327e0df8d571d08b109c2
Imam Hossain <ihossain6898@gmail.com>
2024-06-09 00:08:29 +0600

    app update

--- lib/views/dashboard_screens/dashboard_screen.dart
+++ lib/views/dashboard_screens/dashboard_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:get/get.dart';
 import 'dashboard_controller.dart';
 import 'package:wedevsdssignment/views/cart_screens/cart_screen.dart';
@@ -22,69 +21,95 @@ class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Obx(() => Center(
-        child: DashboardController.widgetOptions[controller.selectedIndex.value],
-      )),
+      body: SafeArea(
+        child: Obx(
+              () => Center(
+            child: DashboardController.widgetOptions[controller.selectedIndex.value],
+          ),
+        ),
+      ),
       bottomNavigationBar: Obx(
-            () => Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(10),
-              topRight: Radius.circular(10),
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.08),
-                offset: Offset(0, -10),
-                blurRadius: 14,
-                spreadRadius: 10,
-              ),
-            ],
+            () => ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10.0),
+            topRight: Radius.circular(10.0),
           ),
-          child: CurvedNavigationBar(
-            index: controller.selectedIndex.value,
-            height: 50.0,
-            items: <Widget>[
-              SvgPicture.asset(
-                'assets/svg/home_icon.svg',
-                height: 24,
-                width: 24,
-                color: controller.selectedIndex.value == 0 ? Colors.white : Colors.black,
-              ),
-              SvgPicture.asset(
-                'assets/svg/category_icon.svg',
-                height: 24,
-                width: 24,
-                color: controller.selectedIndex.value == 1 ? Colors.white : Colors.black,
-              ),
-              SvgPicture.asset(
-                'assets/svg/search_icon.svg',
-                height: 24,
-                width: 24,
-                color: controller.selectedIndex.value == 2 ? Colors.white : Colors.black,
-              ),
-              SvgPicture.asset(
-                'assets/svg/cart_icon.svg',
-                height: 24,
-                width: 24,
-                color: controller.selectedIndex.value == 3 ? Colors.white : Colors.black,
-              ),
-              SvgPicture.asset(
-                'assets/svg/person.svg',
-                height: 24,
-                width: 24,
-                color: controller.selectedIndex.value == 4 ? Colors.white : Colors.black,
-              ),
-            ],
+          child: BottomAppBar(
+            height: 60,
+            elevation: 4,
+            shadowColor: Colors.grey,
+            surfaceTintColor: Colors.white,
             color: Colors.white,
-            buttonBackgroundColor: Colors.red,
-            backgroundColor: Colors.transparent,
-            animationCurve: Curves.easeInOut,
-            animationDuration: Duration(milliseconds: 600),
-            onTap: (index) {
-              controller.onItemTapped(index);
-            },
+            shape: CircularNotchedRectangle(),
+            notchMargin: 5.1,
+            clipBehavior: Clip.antiAlias,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                InkWell(
+                  onTap: () {
+                    controller.onItemTapped(0);
+                  },
+                  child: SvgPicture.asset(
+                    'assets/svg/home_icon.svg',
+                    height: 24,
+                    width: 24,
+                    color: controller.selectedIndex.value == 0 ? Colors.red : Colors.black,
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    controller.onItemTapped(1);
+                  },
+                  child: SvgPicture.asset(
+                    'assets/svg/category_icon.svg',
+                    height: 24,
+                    width: 24,
+                    color: controller.selectedIndex.value == 1 ? Colors.red : Colors.black,
+                  ),
+                ),
+                SizedBox(width: 24), // To create space for the floating action button
+                InkWell(
+                  onTap: () {
+                    controller.onItemTapped(3);
+                  },
+                  child: SvgPicture.asset(
+                    'assets/svg/cart_icon.svg',
+                    height: 24,
+                    width: 24,
+                    color: controller.selectedIndex.value == 3 ? Colors.red : Colors.black,
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    controller.onItemTapped(4);
+                  },
+                  child: SvgPicture.asset(
+                    'assets/svg/person.svg',
+                    height: 24,
+                    width: 24,
+                    color: controller.selectedIndex.value == 4 ? Colors.red : Colors.black,
+                  ),
+                ),
+              ],
+            ),
           ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FloatingActionButton(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(40))),
+          hoverElevation: 10,
+          splashColor: Colors.grey,
+          backgroundColor: Colors.red,
+          tooltip: 'Search',
+          elevation: 4,
+          child: Icon(Icons.search),
+          onPressed: () => setState(() {
+            controller.onItemTapped(2);
+          }),
         ),
       ),
     );
